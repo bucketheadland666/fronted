4e9c71a64ca8f9e2970f8e488e882fb0f380a46b
bucketheadland666 <85567892+bucketheadland666@users.noreply.github.com>
2021-07-19 20:52:18 -0500

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts-form',
+    loadChildren: () => import('./posts-form/posts-form.module').then( m => m.PostsFormPageModule)
+  },
+
+
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
